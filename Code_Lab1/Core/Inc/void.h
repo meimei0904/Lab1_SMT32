3d37ea206aad6717b2b004949ea0adaa43aeeb3b
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2024-09-17 19:25:49 +0700

    Init Lab1_STM32

---
+++ Code_Lab1/Core/Inc/void.h
@@ -0,0 +1,14 @@
+/*
+ * void.h
+ *
+ *  Created on: Sep 17, 2024
+ *      Author: MY LE
+ */
+
+#ifndef INC_VOID_H_
+#define INC_VOID_H_
+#include "main.h"
+//----------------------------------khai bao ham-------------------------------
+
+
+#endif /* INC_VOID_H_ */
---
+++ Code_Lab1/Core/Src/void.c
@@ -0,0 +1,13 @@
+/*
+ * void.c
+ *
+ *  Created on: Sep 17, 2024
+ *      Author: MY LE
+ */
+
+#ifndef SRC_VOID_C_
+#define SRC_VOID_C_
+#include "void.h"
+
+
+#endif /* SRC_VOID_C_ */
